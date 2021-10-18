06565722feedc1660b61fbff200b8fbe4895ccc6
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-10-17 20:11:05 -0400

    About page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import "./styles.css";
 // Import Route and our components
 import {Route, Switch} from "react-router-dom";
 import Nav from "./components/Nav.js";
-
-
+import About from "./pages/About.js"
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Switch>
-
+        <Route path="/about">
+          <About />
+        </Route>
       </Switch>
     </div>
   );
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About () {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>This website is about stocks. I hope you hop on 🚀 and come with me to the moon! </p>
+        </div>
+    )
+}
+
+export default About;
